9c3ac73c40a6de5cb6d86f2dcf8e016147dc0d69
Your Name <your-email@example.com>
2024-12-19 00:39:18 +0100

    Update script.js and style.css

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 // To toggle the navigation menu visibility on hamburger when clicked
 const hamburgerMenu = document.getElementById('hamburger-menu');
-const navMenu = document.querySelector('nav ul', '.btn');
+// const loginButton = document.querySelector('.login-button');
+// const registerButton = document.querySelector('.register-button');
+const navMenu = document.querySelector('nav ul');
+const btnMenu = document.querySelector('.btn');
 const dropdownItems = document.querySelectorAll('.dropdown');
 const dropdownContents = document.querySelectorAll('.dropdown-content');
+let menuOpen = false;
 
 // To toggle the visibility of the main menu on mobile
 hamburgerMenu.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    btnMenu.classList.toggle('hidden'); // Hide .btn menu when mobile menu is active
+
+    if (!menuOpen) {
+    hamburgerMenu.innerHTML = '<img src="images/icon-close-menu.svg" alt="Close">';
+    menuOpen = true;
+    } else {
+    hamburgerMenu.innerHTML = '<img src="images/icon-menu.svg" alt="Menu">';
+    menuOpen = false;
+    }
 });
 
 // This is to toggle the visibility of each dropdown on mobile
