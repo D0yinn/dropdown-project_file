6e2168b8dc60979870cc20099869f5387c4d66ab
Your Name <your-email@example.com>
2024-12-19 09:53:54 +0100

    Update index.html, script.js and style.css

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const hamburgerMenu = document.getElementById('hamburger-menu');
 // const loginButton = document.querySelector('.login-button');
 // const registerButton = document.querySelector('.register-button');
 const navMenu = document.querySelector('nav ul');
-const btnMenu = document.querySelector('.btn');
+const btnMenu = document.querySelector('#btn');
 const dropdownItems = document.querySelectorAll('.dropdown');
 const dropdownContents = document.querySelectorAll('.dropdown-content');
 let menuOpen = false;
@@ -11,7 +11,7 @@ let menuOpen = false;
 // To toggle the visibility of the main menu on mobile
 hamburgerMenu.addEventListener('click', () => {
     navMenu.classList.toggle('active');
-    btnMenu.classList.toggle('hidden'); // Hide .btn menu when mobile menu is active
+    btnMenu.classList.toggle('hidden'); // Hide #btn menu when mobile menu is active
 
     if (!menuOpen) {
     hamburgerMenu.innerHTML = '<img src="images/icon-close-menu.svg" alt="Close">';
